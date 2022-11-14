0c980eafbab897c77b44dd53da08e949118ef189
ezeisrael61c <ezeisrael61c@gmail.com>
2022-11-14 20:30:58 -0300

    quee hice

--- sumar.js
+++ sumar.js
@@ -1,3 +1,3 @@
-let sumar = (...numbers) => numbers.reduce((acum, num) => acum + num)
+let sumar = (...numbers) => numbers.reduce((acum, num) => acum - num)
 
-module.exports = sumar;
+module.exports = sumar;  // git add . /agregra todos los archivos/ git commit -m "mensaje" /comitea los cambios hechos/
